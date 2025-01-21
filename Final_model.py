5ca0215d4d5bbd42ec95225ea5c640cf2c621cba
coalabear07 <127911919+coalabear07@users.noreply.github.com>
2025-01-21 16:57:36 +0800

    Final_model.py

--- Final_model.py
+++ Final_model.py
@@ -10,6 +10,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import numpy as np
 import gdown
+import os
 
 # Function to download files from Google Drive
 def download_file_from_drive(file_url, output):
